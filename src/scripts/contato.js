e151accff4657c7f227c3a74e5d47b8f6c4c321f
pablo <pablocorreia2012@gmail.com>
2021-11-28 14:49:43 -0300

    style guide no contato.js

--- src/scripts/contato.js
+++ src/scripts/contato.js
@@ -1,13 +1,14 @@
 
-var up = document.getElementById("InputName");
+var up = document.getElementById('InputName');
 
 // função para deixar a primeira letra do nome em maiusculo com onblur
 function nameUp() {
-    up.value = up.value[0].toUpperCase() +  up.value.substr(1)
+    'use strict';
+    up.value = up.value[0].toUpperCase() +  up.value.substr(1);
 }
 
 
-let myButton = document.getElementsByTagName('button')[0]
+let myButton = document.getElementsByTagName('button')[0];
 
 
 // console.log(myButton)
@@ -15,6 +16,7 @@ let myButton = document.getElementsByTagName('button')[0]
 
 myButton.onclick = function(event) {
     // event = arguments[0]
-    alert("Caso queira saber se voce segurou o ctrl enquanto apertava o botão a resposta é: " 
-            + event.ctrlKey)
-}
+    'use strict';
+    alert('Caso queira saber se voce segurou o ctrl enquanto apertava o botão a resposta é: ' + 
+            event.ctrlKey);
+};
