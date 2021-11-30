b348d521452f03abd651bba28cdfbb0d946b6c28
pablo <pablocorreia2012@gmail.com>
2021-11-30 15:44:31 -0300

    adicionado novas funçoes

--- src/scripts/contato.js
+++ src/scripts/contato.js
@@ -1,13 +1,17 @@
 
 var up = document.getElementById('InputName');
 
+let formInit = document.getElementById('contato');
+
+let inputName1 = document.getElementById('InputName');
+
 // função para deixar a primeira letra do nome em maiusculo com onblur
 function nameUp() {
     'use strict';
     up.value = up.value[0].toUpperCase() +  up.value.substr(1);
 }
 
-
+// console.log("teste: ",inputName1.value)
 let myButton = document.getElementsByTagName('button')[0];
 
 
@@ -20,3 +24,12 @@ myButton.onclick = function(event) {
     alert('Caso queira saber se voce segurou o ctrl enquanto apertava o botão a resposta é: ' + 
             event.ctrlKey);
 };
+
+
+formInit.onblur = () =>{
+        if (inputName1.value === "") {
+            inputName1.classList.add('erros')
+        }else {
+            inputName1.classList.remove('erros')
+        }
+}
