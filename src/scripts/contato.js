b87c0287f91344535cba2004b7c1e3ed3f6e9a4d
pablo <pablocorreia2012@gmail.com>
2021-11-30 22:55:31 -0300

    adicionado mais algumas validaçoes no formulario

--- src/scripts/contato.js
+++ src/scripts/contato.js
@@ -5,11 +5,13 @@ let formInit = document.getElementById('contato');
 
 let inputName1 = document.getElementById('InputName');
 
+let radioValid = document.getElementById('inlineRadio');
+
 // função para deixar a primeira letra do nome em maiusculo com onblur
-function nameUp() {
-    'use strict';
-    up.value = up.value[0].toUpperCase() +  up.value.substr(1);
-}
+// function nameUp() {
+//     'use strict';
+//     up.value = up.value[0].toUpperCase() +  up.value.substr(1);
+// }
 
 // console.log("teste: ",inputName1.value)
 let myButton = document.getElementsByTagName('button')[0];
@@ -18,18 +20,63 @@ let myButton = document.getElementsByTagName('button')[0];
 // console.log(myButton)
 
 
-myButton.onclick = function(event) {
-    // event = arguments[0]
-    'use strict';
-    alert('Caso queira saber se voce segurou o ctrl enquanto apertava o botão a resposta é: ' + 
-            event.ctrlKey);
-};
+// myButton.onclick = function(event) {
+//     // event = arguments[0]
+//     'use strict';
+//     alert('Caso queira saber se voce segurou o ctrl enquanto apertava o botão a resposta é: ' + 
+//             event.ctrlKey);
+// };
 
-
-formInit.onblur = () =>{
-        if (inputName1.value === "") {
-            inputName1.classList.add('erros')
-        }else {
-            inputName1.classList.remove('erros')
-        }
+function validaCampos() {
+    if (inputName1.value === "") {
+        inputName1.classList.add('erros')
+    }else {
+        up.value = up.value[0].toUpperCase() +  up.value.substr(1);
+        inputName1.classList.remove('erros')
+    }
 }
+// let iChecked = false
+// formInit.onsubmit() = () => {
+//     // let radios = document.getElementById('')
+    
+//     for (const e of radioValid) {
+//         if (e.checked) {
+//             iChecked = true
+//         }
+//     }
+//     if (!iChecked) {
+//         alert("teste")
+//         return false
+//     }
+// }
+
+function validaRadio() {
+    let isCheck = false;
+    let elementos = document.getElementsByName("inlineRadioOptions")
+    // elementos.forEach(element => {
+    //     console.log(element)
+
+    // });
+
+    for (const i in elementos) {
+        if (elementos[i].checked) {
+            isCheck = true;
+            alert("deu certo")
+            return true
+        }else{
+            alert("ERRADO")
+            return false
+        }
+    }
+    // if (!elementos) {
+    //     // isCheck = false;
+    //         alert("ERRADO")
+    //         return false
+    // }
+    
+}
+
+// console.log(document.forms[0].inlineRadio[0])
+// console.log(document.getElementsByName("inlineRadioOptions"))
+
+
