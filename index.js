b9e35650924e326c6a0377e86337cb7eaacee547
pablo <pablocorreia2012@gmail.com>
2021-12-12 16:04:48 -0300

    feito as alteraçoes para o back end

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Faaala DEV! Essa é nossa primeira Aplicação Node...');
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+'/src/index.html'));
+})
+
+app.use('/', router);
+app.use(express.static(__dirname + '/src/css'));
+app.use(express.static(__dirname + '/src/scripts'));
+app.use(express.static(__dirname + '/src/img'));
+
+
+
+app.listen(process.env.port || 3000);
+
+console.log('server rodando')
