1ecb6797cde12451256e66e5d189c8bbe8c35751
pablo <pablocorreia2012@gmail.com>
2021-12-12 17:06:02 -0300

    feito todas as modificaçoes para que o projeto tivesse um backend funcional com rotas no nodeJS

--- index.js
+++ index.js
@@ -7,6 +7,22 @@ router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname+'/src/index.html'));
 })
 
+router.get('/sobre', function (req, res) {
+    res.sendFile(path.join(__dirname+'/src/sobre.html'));
+})
+
+router.get('/contato', function (req, res) {
+    res.sendFile(path.join(__dirname+'/src/contato.html'));
+})
+
+router.get('/trabalhoP', function (req, res) {
+    res.sendFile(path.join(__dirname+'/src/trab-paisagens.html'));
+})
+
+router.get('/trabalhoCar', function (req, res) {
+    res.sendFile(path.join(__dirname+'/src/trab-carros.html'));
+})
+
 app.use('/', router);
 app.use(express.static(__dirname + '/src/css'));
 app.use(express.static(__dirname + '/src/scripts'));
